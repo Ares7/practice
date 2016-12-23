1b25069a7ea17f902b72c7f7db6de05de6e5d392
Sergey Nasonov <sergey.nasonov@tum.de>
2016-12-23 21:36:02 +0100

    Added 1st practise exercise on SelectionSort.

---
+++ SelectionSort/SelectionSort1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int m[]={5,6,7,34,67,3,56,89,67,23,45,6,6,3,56,34,67,3,2,2,3,5,8,9};
+	int s = sizeof(m)/sizeof(int) ;
+
+	for (int i = 0; i < s; ++i)
+	{
+		int min=i;
+		for (int j = i; j < s; ++j)
+		{
+			if (m[j] < m[min])
+			{
+				min=j;
+			}
+		}
+		
+		if(min!=i)
+		{
+			int tmp=m[i];
+			m[i]=m[min];
+			m[min]=tmp;	
+		}
+		
+	}
+
+	for (int i = 0; i < s; ++i)
+	{
+		cout<< m[i]<< ", ";
+	}
+	return 0;
+}
