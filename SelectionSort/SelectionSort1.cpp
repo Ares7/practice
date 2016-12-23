a1a70653967978beb374e32b91e662227886b111
Sergey Nasonov <sergey.nasonov@tum.de>
2016-12-23 21:37:05 +0100

    added TODO Tag.

--- SelectionSort/SelectionSort1.cpp
+++ SelectionSort/SelectionSort1.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+/*
+TODO: check for timing w and w/o additional ifs
+*/
+
 int main(int argc, char const *argv[])
 {
 	int m[]={5,6,7,34,67,3,56,89,67,23,45,6,6,3,56,34,67,3,2,2,3,5,8,9};
